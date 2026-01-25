181817493d3a2409e736232824d7dc488bea8dee
Shahzad Naser <shahzadnaser1122@gmail.com>
2026-01-25 08:14:28 +0500

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Ewarehouse
+
+E-Warehouse
+
+#### License
+
+MIT
---
+++ ewarehouse/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Ewarehouse",
+			"type": "module",
+			"label": _("Ewarehouse")
+		}
+	]
