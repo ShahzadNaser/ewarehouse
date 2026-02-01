aa0d557b1dc4a6720e680d175423ae86b60a63ce
Shahzad <shahzadnasir1122@gmail.com>
2026-02-01 07:05:39 +0500

    Update autonaming.py
    Update series as per currenet date

--- ewarehouse/hook/autonaming.py
+++ ewarehouse/hook/autonaming.py
@@ -2,13 +2,13 @@ import frappe
 from frappe.model.naming import make_autoname
 
 def po(doc, method):
-    cdate = frappe.utils.formatdate(frappe.utils.getdate(doc.get("transaction_date")),"YYMMDD")
+    cdate = frappe.utils.formatdate(frappe.utils.getdate(),"YYMMDD")
     doc.name = make_autoname('PO-.{}-.####'.format(cdate))
 
 def pr(doc, method):
-    cdate = frappe.utils.formatdate(frappe.utils.getdate(doc.get("posting_date")),"YYMMDD")
+    cdate = frappe.utils.formatdate(frappe.utils.getdate(),"YYMMDD")
     doc.name = make_autoname('PR-.{}-.####'.format(cdate))
 
 def pi(doc, method):
-    cdate = frappe.utils.formatdate(frappe.utils.getdate(doc.get("posting_date")),"YYMMDD")
+    cdate = frappe.utils.formatdate(frappe.utils.getdate(),"YYMMDD")
     doc.name = make_autoname('PI-.{}-.####'.format(cdate))
